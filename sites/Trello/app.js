820f4fc44fd23c57edf5e774f8a7261407bcb1d4
Joey Korkames <kfix@users.noreply.github.com>
2015-06-19 14:13:15 -0700

    Trello.app: perform searches using JS

--- sites/Trello/app.js
+++ sites/Trello/app.js
@@ -3,18 +3,27 @@
 "use strict";
 
 var delegate = {}; // our delegate to receive events from the webview app
-var trello = {
+var trelloTab, trello = {
 	transparent: true,
 	url: "https://trello.com",
-	postinject: ['styler'], 
-	preinject: ['notifier'], 
+	postinject: ['styler'],
+	preinject: ['notifier'],
 	subscribeTo: ['TrelloNotification', "MacPinPollStates"] //styler.js does polls
 };
+trelloTab = $.browser.tabSelected = new $.WebView(trello);
 
 function search(query) {
-	$.browser.tabSelected = new $.WebView({url: "https://trello.com/search?q="+query, postinject: ['styler']});
+	//$.browser.tabSelected = new $.WebView({url: trello.url + "/search?q="+query, postinject: ['styler']});
 	// will prompt user to search boards/cards for phrase
-	// would like to hook native JS to perform search instead
+
+	trelloTab.evalJS( // hook app.js to perform search
+		//"document.getElementsByClassName('js-open-search-page')[0].click();" + // goes to dedicated search screen instead of in-header
+		"document.getElementsByClassName('js-search-input')[0].focus();" +
+		"document.getElementsByClassName('js-search-input')[0].value = '"+query+"';" +
+		"var kev = new KeyboardEvent('keydown', { bubbles: true, cancelable: true, view: window, detail: 0, keyIdentifier: 'enter', location: KeyboardEvent.DOM_KEY_LOCATION_STANDARD, ctrlKey: false, altKey: false, shiftKey: false, metaKey: false });" +
+		"document.getElementsByClassName('js-search-input')[0].dispatchEvent(kev);" // ^ fire keydown event to start search
+		//"document.getElementsByClassName('js-search-suggestions-list')[0].focus();"
+ 	);
 }
 
 delegate.launchURL = function(url) {
@@ -23,7 +32,9 @@ delegate.launchURL = function(url) {
 		addr = comps.shift();
 	switch (scheme + ':') {
 		case 'trello:':
-			search(addr);
+			$.browser.unhideApp();
+			$.browser.tabSelected = trelloTab;
+			search(decodeURI(addr));
 			break;
 		default:
 	}
@@ -53,9 +64,17 @@ delegate.decideNavigationForURL = function(url) {
 
 delegate.handleUserInputtedInvalidURL = function(query) {
 	// assuming the invalid url is a search request
-	search(encodeURI(query));
+	search(query);
 	return true; // tell MacPin to stop validating the URL
 };
+
+// handles all URLs drag-and-dropped into NON-html5 parts of Trello and Dock icon.
+delegate.handleDragAndDroppedURLs = function(urls) {
+	console.log(urls);
+	for (var url of urls) {
+		$.browser.tabSelected = new $.WebView({url: url});
+	}
+}
 
 // Fluid API hook should trigger this
 delegate.TrelloNotification = function(tab, msg) {
@@ -69,11 +88,16 @@ delegate.AppFinishedLaunching = function() {
 	$.app.registerURLScheme('trello');
 	$.browser.addShortcut('Trello', trello);
 
-	if ($.launchedWithURL != '') { // app was launched with a feed url
-		this.launchURL($.launchedWithURL);
-		$.launchedWithURL = '';
-	} else {
-		$.browser.tabSelected = new $.WebView(trello);
+	if ($.launchedWithURL != '') { // app was launched with a search query
+		trelloTab.asyncEvalJS( // need to wait for app.js to load and render DOM
+			"true;",
+			5, // delay (in seconds) to wait for tab to load
+			function(result) { // callback
+				delegate.launchURL($.launchedWithURL);
+				$.launchedWithURL = '';
+			}
+		);
 	}
+
 };
 delegate; //return this to macpin
