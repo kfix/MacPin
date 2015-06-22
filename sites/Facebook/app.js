b7f9ae3b5ca4089669a3e1ef64d533dd778c2f4e
Joey Korkames <kfix@users.noreply.github.com>
2015-06-21 19:49:07 -0700

    Facebook: run searches using JS

--- sites/Facebook/app.js
+++ sites/Facebook/app.js
@@ -3,19 +3,25 @@
 "use strict";
 
 var delegate = {}; // our delegate to receive events from the webview app
-var fb = {
+var fbTab, fb = {
 		url: "https://m.facebook.com/home.php",
 		preinject: ['unpreloader'], // this prevents buffering every video in a feed. If you have a fast Mac and Internet, comment out this line
 		postinject: ['styler'],
 		agent: "Mozilla/5.0 (iPad; CPU OS 8_1 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12B410 Safari/600.1.4"
 };
-var fbTab = new $.WebView(fb);
+fbTab = $.browser.tabSelected = new $.WebView(fb);
 $.browser.addShortcut("Facebook Home", fb);
 
 function search(query) {
-	//fbTab.loadURL("https://m.facebook.com/home.php?soft=search");
-	fbTab.loadURL("https://m.facebook.com/graphsearch/str/" + query + "/keywords_top?ref=content_filter&source=typeahead")
-	$.browser.tabSelected = fbTab; // reuse main tab so as not to load fb-js some many times
+	$.browser.tabSelected = fbTab;
+	//fbTab.loadURL("https://m.facebook.com/graphsearch/str/" + query + "/keywords_top?ref=content_filter&source=typeahead")
+	fbTab.evalJS(
+		'document.querySelector("#search_jewel > a").click();' +
+		'document.getElementById("main-search-input").value = "'+query+'";' +
+		'document.getElementById("main-search-input").focus();' +
+		'document.getElementById("main-search-input").dispatchEvent(new Event("input", {bubbles: true, cancelable: true}));'
+	);
+	// document.getElementById('main-search-input')
 }
 
 delegate.launchURL = function(url) {
@@ -27,7 +33,7 @@ delegate.launchURL = function(url) {
 		//case 'facebook:?':
 		case 'fb:':
 		case 'facebook:':
-			search(addr);
+			search(decodeURI(addr));
 			break;
 		default:
 	}
@@ -35,7 +41,7 @@ delegate.launchURL = function(url) {
 
 delegate.handleUserInputtedInvalidURL = function(query) {
 	// assuming the invalid url is a search request
-	search(encodeURI(query));
+	search(query);
 	return true; // tell MacPin to stop validating the URL
 };
 
@@ -44,12 +50,17 @@ delegate.AppFinishedLaunching = function() {
 	//$.app.registerURLScheme('fb'); // claimed by AddressBookUrlForwarder.app http://apple.stackexchange.com/a/105047
 	//`open -a AddressBookUrlForwarder.app fb://profile/4` to see the Zuckster in safari ...
 
-	if ($.launchedWithURL != '') { // app was launched with a feed url
-		this.launchURL($.launchedWithURL);
-		$.launchedWithURL = '';
-	} else {
-		$.browser.tabSelected = fbTab;
+	if ($.launchedWithURL != '') { // app was launched with a search query
+		fbTab.asyncEvalJS( // need to wait for app.js to load and render DOM
+			"true;",
+			6, // delay (in seconds) to wait for tab to load
+			function(result) { // callback
+				delegate.launchURL($.launchedWithURL);
+				$.launchedWithURL = '';
+			}
+		);
 	}
+
 };
 
 delegate; //return this to macpin
