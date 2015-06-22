ef3a6785507a9495f838ef0058a269c05bc67583
Joey Korkames <kfix@users.noreply.github.com>
2015-06-21 19:49:30 -0700

    Facebook: make news feed full-width and toolbar float at top

--- sites/Facebook/styler.js
+++ sites/Facebook/styler.js
@@ -2,7 +2,17 @@
 var css = document.createElement("style");
 css.type = 'text/css';
 css.id = 'macpin'
+
+// make news feed take full width and jewel bar float at top
 css.innerHTML = "\
-			";
+	#header { position: fixed !important; top: 0px !important; }\
+	#main { width: auto !important; }\
+	._e_- { max-width: 10000px !important; }\
+	.mHomeNewsfeed {\
+		margin-left: 0px !important;\
+		margin-right: 0px !important;\
+	}\
+";
+
 document.head.appendChild(css);
 })()
