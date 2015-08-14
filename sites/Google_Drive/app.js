84c09103b18db8b613c6ff4d70c1d456dee27214
Joey Korkames <kfix@noreply.users.github.com>
2015-08-14 16:35:45 -0700

    add beta-state Maps & Drive apps

---
+++ sites/Google_Drive/app.js
@@ -0,0 +1,118 @@
+/*eslint-env applescript*/
+/*eslint eqeqeq:0, quotes:0, space-infix-ops:0, curly:0*/
+"use strict";
+
+var delegate = {}; // our delegate to receive events from the webview app
+var driveTab, drive = {
+	transparent:false,
+	url: "https://drive.google.com",
+	postinject: [],
+	preinject: ['shim_html5_notifications'],
+	subscribeTo: ['receivedHTML5DesktopNotification', "MacPinPollStates"]
+}, driveAlt = {
+	transparent:false,
+	url: "https://drive.google.com/drive/u/1",
+	postinject: [],
+	preinject: ['shim_html5_notifications'],
+	subscribeTo: ['receivedHTML5DesktopNotification', "MacPinPollStates"]
+};
+driveTab = $.browser.tabSelected = new $.WebView(drive);
+
+function search(query) {
+	driveTab.evalJS( // hook app.js to perform search
+		""
+ 	);
+}
+
+delegate.launchURL = function(url) {
+	var comps = url.split(':'),
+		scheme = comps.shift(),
+		addr = comps.shift();
+	switch (scheme + ':') {
+		case 'file':
+			// prompt to do upload
+			driveTab.evalJS('confirm("Upload '+addr+'?");');
+			break;
+		case 'googledrive:':
+		case 'gdrive:':
+			$.browser.unhideApp();
+			$.browser.tabSelected = driveTab;
+			search(decodeURI(addr));
+			break;
+		default:
+	}
+};
+
+delegate.decideNavigationForURL = function(url) {
+	var comps = url.split(':'),
+		scheme = comps.shift(),
+		addr = comps.shift();
+	switch (scheme) {
+		case "http":
+		case "https":
+			if (!~addr.indexOf("//drive.google.com") &&
+				!~addr.indexOf("//accounts.google.com") &&
+				!~addr.indexOf("//docs.google.com") &&
+				!~addr.indexOf(".docs.google.com") &&
+				!~addr.indexOf("//clients6.google.com") &&
+				!~addr.indexOf("//clients5.google.com") &&
+				!~addr.indexOf("//clients4.google.com") &&
+				!~addr.indexOf("//clients3.google.com") &&
+				!~addr.indexOf("//clients2.google.com") &&
+				!~addr.indexOf("//clients1.google.com") &&
+				!~addr.indexOf("//clients.google.com") &&
+				!~addr.indexOf("//0.client-channel.google.com") &&
+				!~addr.indexOf("//www.youtube.com") // yt vids are usually embedded players
+			) {
+				$.app.openURL(url); //pop all external links to system browser
+				console.log("opened "+url+" externally!");
+				return true; //tell webkit to do nothing
+			}
+		case "about":
+		case "file":
+		default:
+			return false;
+	}
+};
+
+delegate.handleUserInputtedInvalidURL = function(query) {
+	// assuming the invalid url is a search request
+	search(query);
+	return true; // tell MacPin to stop validating the URL
+};
+
+// handles all URLs drag-and-dropped into NON-html5 parts of Drive and Dock icon.
+delegate.handleDragAndDroppedURLs = function(urls) {
+	console.log(urls);
+	for (var url of urls) {
+		this.launchURL(url);
+		//$.browser.tabSelected = new $.WebView({url: url});
+	}
+}
+
+delegate.receivedHTML5DesktopNotification = function(tab, note) {
+	console.log(Date() + ' [posted HTML5 notification] ' + note);
+	$.app.postHTML5Notification(note);
+};
+
+delegate.handleClickedNotification = function(from, url, msg) { $.app.openURL(url); return true; };
+
+delegate.AppFinishedLaunching = function() {
+	$.app.registerURLScheme('gdrive');
+	//$.app.registerURLScheme('googledrive'); //iOS
+	$.browser.addShortcut('Google Drive', drive);
+	$.browser.addShortcut('Google Drive (using secondary account)', driveAlt);
+
+	if ($.launchedWithURL != '') { // app was launched with a search query
+		driveTab.asyncEvalJS( // need to wait for app.js to load and render DOM
+			"true;",
+			5, // delay (in seconds) to wait for tab to load
+			function(result) { // callback
+				delegate.launchURL($.launchedWithURL);
+				$.launchedWithURL = '';
+			}
+		);
+	}
+
+};
+delegate; //return this to macpin
---
+++ sites/Google_Maps/app.js
@@ -0,0 +1,125 @@
+/*eslint-env applescript*/
+/*eslint eqeqeq:0, quotes:0, space-infix-ops:0, curly:0*/
+"use strict";
+
+var delegate = {}; // our delegate to receive events from the webview app
+var mapsTab, maps = {
+	url: "https://maps.google.com",
+	postinject: [],
+	preinject: ['shim_html5_notifications', 'shim_html5_geolocation'],
+	subscribeTo: ['receivedHTML5DesktopNotification', "MacPinPollStates", "getGeolocation", "watchGeolocation, deactivateGeolocation"]
+}, mapsAlt = {
+	url: "https://www.google.com/maps/?authuser=1",
+	postinject: [],
+	preinject: ['shim_html5_notifications', 'shim_html5_geolocation'],
+	subscribeTo: ['receivedHTML5DesktopNotification', "MacPinPollStates", "getGeolocation", "watchGeolocation, deactivateGeolocation"]
+};
+
+mapsTab = $.browser.tabSelected = new $.WebView(maps);
+
+function search(query) {
+	// https://developers.google.com/maps/documentation/ios/urlscheme#search
+	var mapper = (~$.browser.tabSelected.url.indexOf('//www.google.com/maps/')) ? $.browser.tabSelected : mapsTab;
+	mapper.evalJS( // hook app.js to perform search
+		"document.querySelector('input[aria-label=Search]').value = '" + query + "';" + 
+		 "document.getElementById('searchbox_form').submit();"
+		//"document.getElementById('searchbox_form').dispatchEvent(new Event('submit', {bubbles: true, cancelable: true}));"
+		// "document.querySelector('button[aria-label=Search]').click();"
+		// append '?q='+encodeURI(query)
+ 	);
+}
+
+delegate.launchURL = function(url) {
+	var comps = url.split(':'),
+		scheme = comps.shift(),
+		addr = comps.shift();
+	switch (scheme + ':') {
+		case 'googlemaps:':
+		case 'gmaps:':
+			$.browser.unhideApp();
+			$.browser.tabSelected = mapsTab;
+			search(decodeURI(addr));
+			break;
+		default:
+	}
+};
+
+delegate.decideNavigationForURL = function(url) {
+	var comps = url.split(':'),
+		scheme = comps.shift(),
+		addr = comps.shift();
+	switch (scheme) {
+		case "http":
+		case "https":
+			if (!~addr.indexOf("//maps.google.com") &&
+				!~addr.indexOf("//accounts.google.com") &&
+				!~addr.indexOf("//places.google.com") &&
+				!~addr.indexOf("//plus.google.com") &&
+				!~addr.indexOf("//www.google.com/maps/") &&
+				!~addr.indexOf("//google-latlong.blogspot.com") &&
+				!~addr.indexOf("//gokml.net") &&
+				!~addr.indexOf("//www.youtube.com") // yt vids are usually embedded players
+			) {
+				$.app.openURL(url); //pop all external links to system browser
+				console.log("opened "+url+" externally!");
+				return true; //tell webkit to do nothing
+			}
+		case "about":
+		case "file":
+		default:
+			return false;
+	}
+};
+
+delegate.handleUserInputtedInvalidURL = function(query) {
+	// assuming the invalid url is a search request
+	search(query);
+	return true; // tell MacPin to stop validating the URL
+};
+
+// handles all URLs drag-and-dropped into NON-html5 parts of Drive and Dock icon.
+delegate.handleDragAndDroppedURLs = function(urls) {
+	console.log(urls);
+	for (var url of urls) {
+		//$.browser.tabSelected = new $.WebView({url: url});
+	}
+}
+
+delegate.receivedHTML5DesktopNotification = function(tab, note) {
+	console.log(Date() + ' [posted HTML5 notification] ' + note);
+	$.app.postHTML5Notification(note);
+};
+
+delegate.handleClickedNotification = function(from, url, msg) { $.app.openURL(url); return true; };
+
+//delegate.getGeolocation = function(tab, opts) { $.app.getGeolocation(); };
+
+//delegate.watchGeolocation = function(tab, msg) { $.app.watchGeolocation(); }; // msg = [opts, id]
+
+//delegate.deactivateGeolocation = function(tab, id) { $.app.deactivateGeolocation(id); };
+
+delegate.updateGeolocation = function(lat, lon) {
+	// fire custom geolocation event in mapsTab (or all tabs?) with lat, lon
+}
+
+delegate.AppFinishedLaunching = function() {
+	$.app.registerURLScheme('gmaps');
+	//$.app.registerURLScheme('googlemaps'); //IOS?
+	$.browser.addShortcut('Google Maps', maps);
+	$.browser.addShortcut('Google Maps (using secondary account)', mapsAlt);
+	$.browser.addShortcut('Google Maps Dev Team blog', "http://google-latlong.blogspot.com");
+	$.browser.addShortcut('Classic gMaps', "http://gokml.net/maps");
+
+	if ($.launchedWithURL != '') { // app was launched with a search query
+		mapsTab.asyncEvalJS( // need to wait for app.js to load and render DOM
+			"true;",
+			5, // delay (in seconds) to wait for tab to load
+			function(result) { // callback
+				delegate.launchURL($.launchedWithURL);
+				$.launchedWithURL = '';
+			}
+		);
+	}
+
+};
+delegate; //return this to macpin
