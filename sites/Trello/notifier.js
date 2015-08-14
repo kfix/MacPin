adc5e127c2907344c1ace4d1da1633fe71f141de
Joey Korkames <kfix@noreply.users.github.com>
2015-08-14 16:33:55 -0700

    excess JS

--- sites/Trello/notifier.js
+++
@@ -1,9 +0,0 @@
-/*eslint eqeqeq:0, quotes:0, space-infix-ops:0, curly:0*/
-/*globals webkit*/
-"use strict";
-
-// https://gist.github.com/marclipovsky/4566822
-var fluid = {}; // http://www.fluidapp.com/developer/
-fluid.showGrowlNotification = function(h) { //h{} :title, :priority, :sticky, :description, :icon, :callback
-	webkit.messageHandlers.TrelloNotification.postMessage([h.title, "", h.description]);
-};
