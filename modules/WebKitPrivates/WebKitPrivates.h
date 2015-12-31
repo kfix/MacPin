f16b0b69da918e56c75d11d92d9346611716ef24
Joey Korkames <kfix@users.noreply.github.com>
2015-12-30 17:42:46 -0800

    add private JSContext headers

--- modules/WebKitPrivates/WebKitPrivates.h
+++ modules/WebKitPrivates/WebKitPrivates.h
@@ -8,6 +8,7 @@
 #import "_WKDownload.h"
 #import "_WKDownloadDelegate.h"
 #import "WKProcessPoolPrivate.h"
+#import "JSContext+Privates.h"
 
 #if TARGET_OS_IPHONE || TARGET_IPHONE_SIMULATOR || TARGET_OS_SIMULATOR
 // no DnD on iOS
