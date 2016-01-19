894b664042d113096eadad16f46c2497fc6351ea
Joey Korkames <kfix@users.noreply.github.com>
2016-01-19 01:17:39 -0800

    add gesture notes

--- templates/Resources/shim_touchpad_gestures.js
+++ templates/Resources/shim_touchpad_gestures.js
@@ -4,3 +4,9 @@
 //     https://developer.mozilla.org/en-US/docs/Web/Guide/Events/Mouse_gesture_event://developer.mozilla.org/en-US/docs/Web/Guide/Events/Mouse_gesture_events 
 // or maybe use current mouse pointer as a virtual "center" of the gesture and fully mimic TouchEvents?
 //    hold down ctrl+cmd to bypass/trigger virtual centering 
+// Apple is way ahead of me:
+//    https://developer.mozilla.org/en-US/docs/Web/API/GestureEvent
+//    https://developer.apple.com/library/iad/documentation/UserExperience/Reference/GestureEventClassReference/index.html
+//    https://github.com/taye/interact.js
+//    https://github.com/WebKit/webkit/commit/10f11d2d8227a7665c410c642ebb6cb9d482bfa8 Web Gesture events for Mac
+//    https://github.com/WebKit/webkit/commit/d4d57e4697410ebaf006f3c62b04038a1915ca7f enable Web Gestures on Mac
