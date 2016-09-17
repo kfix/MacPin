b49f14a8620a755f74671de1004617d7907c2dc8
Joey Korkames <kfix@users.noreply.github.com>
2016-09-17 13:55:18 -0700

    Slack: add a dark mode

--- sites/Slack/app.js
+++ sites/Slack/app.js
@@ -1,14 +1,15 @@
 /*eslint-env applescript*/
 /*eslint-env builtins*/
+/*eslint-env es6*/
 /*eslint eqeqeq:0, quotes:0, space-infix-ops:0, curly:0*/
-"use strict";
+//"use strict";
 
 // official Slack.app uses MacGap
 
 var slackTab, slack = {
 	url: 'https://slack.com/signin/',
 	subscribeTo: ['receivedHTML5DesktopNotification', "MacPinPollStates"],
-	agent: '"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/601.7.7 (KHTML, like Gecko) Slack_SSB/2.0.3"',
+	agent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/601.7.7 (KHTML, like Gecko) Slack_SSB/2.0.3',
 	preinject: ['shim_html5_notifications']
 };
 $.browser.tabSelected = slackTab = new $.WebView(slack);
@@ -64,9 +65,19 @@ delegate.decideNavigationForURL = function(url) {
 	}
 };
 
+// $.app.loadAppScript(`file://${$.app.resourcePath}/enDarken.js`);
+// ^^ uses CSS filters, CPU intensive
+delegate.enDarken = function(tab) {
+	let idx = tab.styles.indexOf('drewDark');
+	(idx >= 0) ? tab.popStyle(idx) : tab.style('drewDark');
+	return;
+};
+
 delegate.AppFinishedLaunching = function() {
 	//$.browser.addShortcut('Slack', slack);
 	// FIXME: use LocalStorage to save slackTab's team-domain after sign-in, and restore that on every start up
-	// FIXME add themes: https://github.com/fishman/slack
+	$.browser.addShortcut('Dark Mode', ['enDarken']);
+	// FIXME add themes: https://gist.github.com/DrewML/0acd2e389492e7d9d6be63386d75dd99
 };
+
 delegate; //return this to macpin
