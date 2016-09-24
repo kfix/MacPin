9156054e76df09dc540cee46e2d2a9d4d8ed2681
Joey Korkames <kfix@users.noreply.github.com>
2016-09-23 17:25:52 -0700

    JupyterLab: new app

---
+++ sites/JupyterLab/app.js
@@ -0,0 +1,33 @@
+/*eslint-env applescript*/
+/*eslint-env builtins*/
+/*eslint eqeqeq:0, quotes:0, space-infix-ops:0, curly:0*/
+"use strict";
+
+// need derivative logo: https://github.com/jupyter/design/issues/21
+//   or overlay netloc on dock icon at runtime ?
+
+var lab = {url: "https://joey-lxd:8888"};
+// FIXME: source from ENV or NSUserDefaults...
+// could also grab system's username and add "-lab" to it...
+
+var delegate = {}; // our delegate to receive events from the webview app
+
+delegate.launchURL = function(url) {
+	console.log("app.js: launching " + url);
+	var comps = url.split(':'),
+		scheme = comps.shift(),
+		addr = comps.shift();
+	switch (scheme + ':') {
+		default:
+			$.browser.tabSelected = new $.WebView({url: url});
+	}
+};
+
+delegate.AppFinishedLaunching = function() {
+	$.browser.addShortcut('JuyterLab', lab);
+	$.browser.addShortcut('Dark Mode', ['enDarken']);
+	$.browser.tabSelected = new $.WebView(lab);
+};
+$.app.loadAppScript(`file://${$.app.resourcePath}/enDarken.js`);
+
+delegate; //return this to macpin
