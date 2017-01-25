b00f3dde1a109fc12f73e5f1636e98a44536b07a
Joey Korkames <kfix@users.noreply.github.com>
2017-01-24 18:29:01 -0800

    URLProtocol: link to helpful gist

--- modules/MacPin/URLProtocol.swift
+++ modules/MacPin/URLProtocol.swift
@@ -1,5 +1,7 @@
 import Foundation
+
 class MPURLProtocol: NSURLProtocol {
+	// https://gist.github.com/omerlh/2617db3d4116bf46bdeb
 	class func canInitWithRequest(request: NSURLRequest!) -> Bool {
 		warn()
 	    return false // FIXME: WK always throws away the post body: https://bugs.webkit.org/show_bug.cgi?id=138169i#c27
