kfix/MacPin

commit 7be2c11e8b22c93c12ea075ae9cd2cdd1116694b

make: use current Xcode.app (9.3)

modules/MacPin/OSX/AppDelegateOSX.swift
@@ -391,8 +391,8 @@ extension MacPinAppDelegateOSX: NSUserNotificationCenterDelegate {
 		warn("user clicked notification")
 
 		if AppScriptRuntime.shared.jsdelegate.tryFunc("handleClickedNotification",
-			notification.title as NSString, notification.subtitle as NSString,
-			notification.informativeText as NSString, notification.identifier as NSString) {
+			notification.title as! NSString, notification.subtitle as! NSString,
+			notification.informativeText as! NSString, notification.identifier as! NSString) {
 				warn("handleClickedNotification fired!")
 				center.removeDeliveredNotification(notification)
 		}

Makefile
@@ -15,8 +15,10 @@ archs_iphoneos		?= arm64
 target_ver_OSX		?= 10.13
 target_ver_iOS		?= 9.1
 
-xcode				?= /Applications/Xcode8.2.1.app
-# Xcode 8.3 removed swift2 support
+xcode				?= /Applications/Xcode.app
+swifttoolchain		?= XcodeDefault
+swiftver			?= 3
+
 ifneq ($(xcode),)
 xcrun				?= env DEVELOPER_DIR=$(xcode)/Contents/Developer xcrun
 xcs					?= env DEVELOPER_DIR=$(xcode)/Contents/Developer xcode-select
@@ -328,7 +330,7 @@ endif
 
 # need to gen static html with https://github.com/realm/jazzy
 doc stpdoc: $(execs) $(objs)
-	for i in $(build_mods) WebKit WebKitPrivates; do echo ":print_module $$i" | $(env) xcrun swift $(swift) $(debug) $(incdirs) -deprecated-integrated-repl; done
+	for i in $(build_mods) WebKit WebKitPrivates; do echo ":print_module $$i" | $(env) xcrun swift $(swift) $(debug) $(incdirs) -swift-version $(swiftver) -Xfrontend -color-diagnostics -deprecated-integrated-repl; done
 	#xcrun swift-ide-test -print-module -source-filename /dev/null -print-regular-comments -module-to-print Prompt
 	#xcrun swift-ide-test -sdk "$(sdkpath)" -source-filename=. -print-module -module-to-print="Prompt" -synthesize-sugar-on-types -module-print-submodules -print-implicit-attrs
 

eXcode.mk
@@ -136,12 +136,14 @@ verbose				?=
 xcrun				?= xcrun
 xcs					?= xcode-select
 
-swiftver			:= Swift_3_0_2
+swiftver			?= 3
+swifttoolchain		?= XcodeDefault
+# Xcode.app/Contents/Developer/Toolchains/*.xctoolchain/ToolchainInfo.plist << TC ids in there
+
 sdkpath				:= $(shell $(xcrun) --show-sdk-path --sdk $(sdk))
 # https://github.com/apple/swift/blob/master/include/swift/Option/FrontendOptions.td
-swiftc				:= $(xcrun) --toolchain com.apple.dt.toolchain.$(swiftver) -sdk $(sdk) swiftc -target $(arch)-$(target_$(platform)) -Xfrontend -color-diagnostics $(verbose)
-# Xcode.app/Contents/Developer/Toolchains/*.xctoolchain/ToolchainInfo.plist << TC ids in there
-swift-update		:= $(xcrun) --toolchain com.apple.dt.toolchain.Swift_3_0_2 -sdk $(sdk) swift-update -target $(arch)-$(target_$(platform)) $(verbose)
+swiftc				:= $(xcrun) --toolchain com.apple.dt.toolchain.$(swifttoolchain) -sdk $(sdk) swiftc -swift-version $(swiftver) -target $(arch)-$(target_$(platform)) -Xfrontend -color-diagnostics $(verbose)
+swift-update		:= $(xcrun) --toolchain com.apple.dt.toolchain.$(swifttoolchain) -sdk $(sdk) swift-update -swift-version $(swiftver) -target $(arch)-$(target_$(platform)) -Xfrontend -color-diagnostics) $(verbose)
 clang				:= $(xcrun) -sdk $(sdk) clang -fmodules -target $(arch)-$(target_$(platform)) $(verbose)
 clangpp				:= $(xcrun) -sdk $(sdk) clang++ -fmodules -fcxx-modules -std=c++11 -stdlib=libc++ -target $(arch)-$(target_$(platform)) $(verbose)
 #-fobj-arc
@@ -153,10 +155,8 @@ incdirs				+= -I $(sdkpath)/System/Library/Frameworks
 incdirs				+= -I $(outdir)
 os_frameworks		:= -F $(sdkpath)/System/Library/Frameworks -L $(sdkpath)/System/Library/Frameworks
 frameworks			:= -F $(outdir)/Frameworks
-#swifttoolchain		:= Swift_2.3.xctoolchain
-swifttoolchain		:= XcodeDefault.xctoolchain
-swiftlibdir			:= $(lastword $(wildcard /Library/Developer/CommandLineTools/usr/lib/swift/$(sdk) $(shell $(xcs) -p)/Toolchains/$(swifttoolchain)/usr/lib/swift/$(sdk)))
-swiftstaticdir		:= $(lastword $(wildcard /Library/Developer/CommandLineTools/usr/lib/swift_static/$(sdk) $(shell $(xcs) -p)/Toolchains/$(swifttoolchain)/usr/lib/swift_static/$(sdk)))
+swiftlibdir			:= $(lastword $(wildcard /Library/Developer/CommandLineTools/usr/lib/swift/$(sdk) $(shell $(xcs) -p)/Toolchains/$(swifttoolchain).xctoolchain/usr/lib/swift/$(sdk)))
+swiftstaticdir		:= $(lastword $(wildcard /Library/Developer/CommandLineTools/usr/lib/swift_static/$(sdk) $(shell $(xcs) -p)/Toolchains/$(swifttoolchain).xctoolchain/usr/lib/swift_static/$(sdk)))
 $(info [$(eXcode)] compiling against $(sdkpath))
 $(info [$(eXcode)] swift libraries: $(swiftlibdir) $(swiftstaticdir))
 $(info [$(eXcode)] swiftc version: $(shell $(swiftc) --version))

modules/MacPin/WebViewDelegates.swift
@@ -165,10 +165,10 @@ extension WebViewController: WKNavigationDelegate, WKNavigationDelegatePrivate {
 			switch (error._domain, error._code) {
 				// https://developer.apple.com/reference/cfnetwork/cfnetworkerrors
 				// https://developer.apple.com/library/mac/documentation/Networking/Reference/CFNetworkErrors/index.html#//apple_ref/c/tdef/CFNetworkErrors
-				case (kCFErrorDomainCFNetwork as NSString, Int(CFNetworkErrors.cfErrorHTTPProxyConnectionFailure.rawValue)):
+				case (String(kCFErrorDomainCFNetwork), Int(CFNetworkErrors.cfErrorHTTPProxyConnectionFailure.rawValue)):
 					UserDefaults.standard.removeObject(forKey: "WebKit2HTTPProxy") // FIXME: should prompt first
 					fallthrough //webview.flushProcessPool()
-				case (kCFErrorDomainCFNetwork as NSString, Int(CFNetworkErrors.cfErrorHTTPSProxyConnectionFailure.rawValue)):
+				case (String(kCFErrorDomainCFNetwork), Int(CFNetworkErrors.cfErrorHTTPSProxyConnectionFailure.rawValue)):
 					UserDefaults.standard.removeObject(forKey: "WebKit2HTTPSProxy") // FIXME: should prompt first
 					fallthrough ////webview.flushProcessPool()
 				//case (kCFErrorDomainCFNetwork as NSString, Int(CFNetworkErrors.CFURLErrorZeroByteResource.rawValue)):

commit 5d67a1297d2f79860e951064fe62e5c0b6060230

OSX: fix nil-unwrap regression by 7be2c11 for clicked notification

modules/MacPin/OSX/AppDelegateOSX.swift
@@ -391,8 +391,8 @@ extension MacPinAppDelegateOSX: NSUserNotificationCenterDelegate {
 		warn("user clicked notification")
 
 		if AppScriptRuntime.shared.jsdelegate.tryFunc("handleClickedNotification",
-			notification.title as! NSString, notification.subtitle as! NSString,
-			notification.informativeText as! NSString, notification.identifier as! NSString) {
+			(notification.title ?? "") as NSString, (notification.subtitle ?? "") as NSString,
+			(notification.informativeText ?? "") as NSString, (notification.identifier ?? "") as NSString) {
 				warn("handleClickedNotification fired!")
 				center.removeDeliveredNotification(notification)
 		}