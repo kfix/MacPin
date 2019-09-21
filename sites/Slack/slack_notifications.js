90377ac1fa2e55ffd1ddaec01df8c5b7d588f3fb
Joey Korkames <kfix@users.noreply.github.com>
2019-09-21 11:20:38 -0700

    Slack: they've gone full-webpack, no TS object anymore

--- sites/Slack/slack_notifications.js
+++
@@ -1,8 +0,0 @@
-/*
- How nice of Slack to not obfuscate or IIFE their JS!
-*/
-
-// we can't cache user's intention, so just presume they want notifications
-TS.ui.growls.promptForPermission(); // Motification.permission == "granted"
-window.TS.ui.growls.checkPermission = function() { console.log("checked usernote perms!"); return true; }
-// TS.ui.growls.showTestNotification("title","message", () => console.log("test note clicked!") )
