6348a049684d2513f1a2306030cf2f16f1e73aad
Joey Korkames <kfix@users.noreply.github.com>
2019-09-21 17:00:25 -0700

    WhatsApp: remove obsolete user-agent

--- sites/WhatsApp/app.js
+++ sites/WhatsApp/app.js
@@ -5,11 +5,9 @@
 var delegate = {}; // our delegate to receive events from the webview app
 
 delegate.AppFinishedLaunching = function() {
-	    $.browser.tabSelected = new $.WebView({
-			    url: "https://web.whatsapp.com/",
-		    	    agent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/9.0.1 Safari/7046A194A"
-
-		        });
+	$.browser.tabSelected = new $.WebView({
+		url: "https://web.whatsapp.com/"
+	});
 };
 
 delegate; //return this to macpin
