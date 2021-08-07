62752bd0e5df6339d42504e550b24926f2f778af
Joey Korkames <kfix@users.noreply.github.com>
2021-08-07 01:50:49 -0700

    fix extsIOS

--- modules/MacPin/_ios/ExtsIOS.swift
+++ modules/MacPin/_ios/ExtsIOS.swift
@@ -7,31 +7,31 @@ import UIKit
 // show an NSError to the user, attaching it to any given view
 func displayError(_ error: NSError, _ vc: UIViewController? = nil) {
 	warn("`\(error.localizedDescription)` [\(error.domain)] [\(error.code)] `\(error.localizedFailureReason ?? String())` : \(error.userInfo)")
-	let alerter = UIAlertController(title: error.localizedFailureReason, message: error.localizedDescription, preferredStyle: .Alert) //.ActionSheet
-	let OK = UIAlertAction(title: "OK", style: .Default) { (_) in }
+	let alerter = UIAlertController(title: error.localizedFailureReason, message: error.localizedDescription, preferredStyle: .alert) //.ActionSheet
+	let OK = UIAlertAction(title: "OK", style: .default) { (_) in }
 	alerter.addAction(OK)
 
 	if let vc = vc { 
-		vc.presentViewController(alerter, animated: true, completion: nil)
+		vc.present(alerter, animated: true, completion: nil)
 	} else {
-		UIApplication.sharedApplication().keyWindow?.rootViewController?.presentViewController(alerter, animated: true, completion: nil)
+		UIApplication.shared.keyWindow?.rootViewController?.present(alerter, animated: true, completion: nil)
 	}
 }
 
 func askToOpenURL(_ url: NSURL?) {
-	if let url = url where UIApplication.sharedApplication().canOpenURL(url) {
+	if let url = url, UIApplication.shared.canOpenURL(url as URL) {
 		//if {
 		//	// macpin is already the registered handler for this (probably custom) url scheme, so just open it
 		//	UIApplication.sharedApplication().openURL(url)
 		//	return
 		//}
 
-		let alerter = UIAlertController(title: "Open outside MacPin", message: url.description, preferredStyle: .Alert) //.ActionSheet
-		let OK = UIAlertAction(title: "OK", style: .Default) { (_) in UIApplication.sharedApplication().openURL(url) }
+		let alerter = UIAlertController(title: "Open outside MacPin", message: url.description, preferredStyle: .alert) //.ActionSheet
+		let OK = UIAlertAction(title: "OK", style: .default) { (_) in UIApplication.shared.openURL(url as URL) }
 		alerter.addAction(OK)
-		let Cancel = UIAlertAction(title: "Cancel", style: .Cancel) { (_) in }
+		let Cancel = UIAlertAction(title: "Cancel", style: .cancel) { (_) in }
 		alerter.addAction(Cancel)
-		UIApplication.sharedApplication().keyWindow?.rootViewController?.presentViewController(alerter, animated: true, completion: nil)
+		UIApplication.shared.keyWindow?.rootViewController?.present(alerter, animated: true, completion: nil)
 
 		return
 	}
