bb1244742b8ff68d08309a8f56dbeafa278178d6
Joey Korkames <kfix@users.noreply.github.com>
2021-08-07 21:56:29 -0700

    ios: update the main.swift

--- modules/MacPin/_ios/main.swift
+++ modules/MacPin/_ios/main.swift
@@ -1,3 +1,11 @@
 import Foundation
 import UIKit
-UIApplicationMain(CommandLine.argc, CommandLine.unsafeArgv, NSStringFromClass(MacPinApp), NSStringFromClass(MacPinAppDelegatePlatform))
+import MacPin
+UIApplicationMain(
+    CommandLine.argc,
+    CommandLine.unsafeArgv,
+    NSStringFromClass(MacPinApp.self),
+    NSStringFromClass(MacPinAppDelegateIOS.self)
+)
+// FIXME: just annotate a func with @main ?
+// https://swiftrocks.com/entry-points-swift-uiapplicationmain-main
