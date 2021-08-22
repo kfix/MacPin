42dc535ef8f108aacd067effc36e2fa2a6d11f4a
Joey Korkames <kfix@users.noreply.github.com>
2021-08-21 22:06:31 -0700

    ios: fix appdelegate startup
    UX & JS runtime are booting again!
    pages still navigate
    crashes if we remove a tab..
    also crashes when acessing tablist

--- modules/MacPin/_ios/AppDelegateIOS.swift
+++ modules/MacPin/_ios/AppDelegateIOS.swift
@@ -8,53 +8,68 @@ public class MacPinAppDelegateIOS: NSObject, MacPinAppDelegate {
 	var browserController: BrowserViewController = MobileBrowserViewController() //frame: UIScreen.mainScreen().applicationFrame)
 	let window = UIWindow(frame: UIScreen.main.bounds) // total pixels w/ rotation
 
+	var prompter: Prompter? = nil
 	func application(_ application: UIApplication, openURL url: URL, sourceApplication: String?, annotation: AnyObject) -> Bool {
 		warn("`\(url)` -> AppScriptRuntime.shared.jsdelegate.launchURL()")
 		AppScriptRuntime.shared.emit(.launchURL, url.absoluteString ?? "")
 		return true //FIXME
 	}
 
-	public func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [AnyHashable: Any]?) -> Bool { // state not restored, UI not presented
+	public func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool { // state not restored, UI not presented
 		application.registerUserNotificationSettings(UIUserNotificationSettings(types: [UIUserNotificationType.sound, UIUserNotificationType.alert, UIUserNotificationType.badge], categories: nil))
 
 		return true //FIXME
 	}
 
-	public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [AnyHashable: Any]?) -> Bool { //state restored, but UI not presented yet
+	public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool { //state restored, but UI not presented yet
 		// launchOptions: http://nshipster.com/launch-options/
 		UIApplication.shared.statusBarStyle = .lightContent
 		window.backgroundColor = UIColor.white // visible behind status bar area when unobscured by page content
 		window.rootViewController = browserController as! UIViewController //adds the browserView to window.subviews
 		window.makeKeyAndVisible() // presentation is deferred until after didFinishLaunching
+		AppScriptRuntime.shared.emit(.AppFinishedLaunching, "FIXME: provide real launch URLs")
 
 		// airplay to an external screen on a mac or appletv
 		//	https://developer.apple.com/library/ios/documentation/WindowsViews/Conceptual/WindowAndScreenGuide/UsingExternalDisplay/UsingExternalDisplay.html#//apple_ref/doc/uid/TP40012555-CH3-SW1
 
+		warn(CommandLine.arguments.description)
 		for (idx, arg) in CommandLine.arguments.enumerated() {
 			switch (arg) {
 				case "-i":
-					if isatty(1) == 1 { AppScriptRuntime.shared.REPL() } //open a JS console on the terminal, if present
-				case "-t":
-					if isatty(1) == 1 {
-						if idx + 1 >= CommandLine.arguments.count { // no arg after this one
-							browserController.tabs.first?.REPL() //open a JS console for the first tab WebView on the terminal, if present
-							break
-						}
-						if let tabnum = Int(CommandLine.arguments[idx + 1]), browserController.tabs.count >= tabnum { // next argv should be tab number
-							browserController.tabs[tabnum].REPL() // open a JS Console on the requested tab number
-						} else {
-							browserController.tabs.first?.REPL() //open a JS console for the first tab WebView on the terminal, if present
+					//open a JS console on the terminal, if present
+					if AppScriptRuntime.shared.eventCallbacks[.printToREPL, default: []].isEmpty {
+						if let repl_js = Bundle.main.url(forResource: "app_repl", withExtension: "js") {
+							warn("injecting TTY REPL")
+							AppScriptRuntime.shared.eventCallbacks[.printToREPL] = [
+								AppScriptRuntime.shared.loadCommonJSScript(repl_js.absoluteString).objectForKeyedSubscript("repl")
+							]
 						}
 					}
-					// ooh, pretty: https://github.com/Naituw/WBWebViewConsole
-					// https://github.com/Naituw/WBWebViewConsole/blob/master/WBWebViewConsole/Views/WBWebViewConsoleInputView.m
+					prompter = AppScriptRuntime.shared.REPL()
+
+				case "-t":
+					if idx + 1 >= CommandLine.arguments.count { // no arg after this one
+						prompter = browserController.tabs.first?.REPL() //open a JS console for the first tab WebView on the terminal, if present
+						break
+					}
+
+					if let tabnum = Int(CommandLine.arguments[idx + 1]), browserController.tabs.count >= tabnum { // next argv should be tab number
+						prompter = browserController.tabs[tabnum].REPL() // open a JS Console on the requested tab number
+						// FIXME skip one more arg
+					} else {
+						prompter = browserController.tabs.first?.REPL() //open a JS console for the first tab WebView on the terminal, if present
+					}
+
+				case let _ where (arg.hasPrefix("http:") || arg.hasPrefix("https:") || arg.hasPrefix("about:") || arg.hasPrefix("file:")):
+					warn("\(arg)")
+					//application(NSApp, openFile: arg) // LS will openFile AND argv.append() fully qualified URLs
 				default:
 					if arg != CommandLine.arguments[0] && !arg.hasPrefix("-psn_0_") { // Process Serial Number from LaunchServices open()
 						warn("unrecognized argv[]: `\(arg)`")
 					}
 			}
 		}
-
+		prompter?.start()
 		return true //FIXME
   }
 
@@ -62,13 +77,13 @@ public class MacPinAppDelegateIOS: NSObject, MacPinAppDelegate {
 		//if application?.orderedDocuments?.count < 1 { showApplication(self) }
 
 		browserController.view.frame = UIScreen.main.bounds
-
-		if !AppScriptRuntime.shared.context.objectForKeyedSubscript("$").objectForKeyedSubscript("browser").isObject { //first run, not an app restore
-			AppScriptRuntime.shared.context.objectForKeyedSubscript("$").setObject(browserController, forKeyedSubscript: "browser")
+		if !AppScriptRuntime.shared.loadMainScript() { // load main.js, if present
+			self.browserController.extend(AppScriptRuntime.shared.exports) // expose our default browser instance early on, because legacy
+			BrowserController.self.exportSelf(AppScriptRuntime.shared.context.globalObject) // & the type for self-setup
+			AppScriptRuntime.shared.exports.setObject(MPWebView.self, forKeyedSubscript: "WebView" as NSString) // because legacy
+			AppScriptRuntime.shared.exports.setObject("", forKeyedSubscript: "launchedWithURL" as NSString)
 			AppScriptRuntime.shared.loadSiteApp() // load app.js, if present
-			AppScriptRuntime.shared.jsdelegate.tryFunc("AppFinishedLaunching")
-		} else {
-			AppScriptRuntime.shared.jsdelegate.tryFunc("AppRestored")
+			AppScriptRuntime.shared.emit(.AppFinishedLaunching)
 		}
 
 		if browserController.tabs.count < 1 { browserController.newTabPrompt() } //don't allow a tabless state
