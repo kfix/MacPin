7df967a8ff75a0b84fcc2deaddecac427d65e688
Joey Korkames <kfix@users.noreply.github.com>
2022-01-08 15:10:05 -0700

    add missing JSC package

--- modules/JavaScriptCorePrivates/Package.swift
+++ modules/JavaScriptCorePrivates/Package.swift
@@ -2,5 +2,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "JavaScriptCorePrivates"
+    name: "JavaScriptCorePrivates",
+    products: [
+        .library(name: "JavaScriptCorePrivates", targets: ["JavaScriptCorePrivates"]),
+    ],
+    targets: [
+        .systemLibrary(
+            name: "JavaScriptCorePrivates",
+            path: "./" // where the modulemap lives
+        )
+    ]
 )
