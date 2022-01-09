7c6a7f6b994ecc3742eb2cf85a24621f833544f0
Joey Korkames <kfix@users.noreply.github.com>
2022-01-09 11:46:48 -0700

    Twitter: update user agent

--- sites/Twitter/app.js
+++ sites/Twitter/app.js
@@ -8,8 +8,8 @@ var delegate = {}; // our delegate to receive events from the webview app
 delegate.AppFinishedLaunching = function() {
 	$.browser.addShortcut('stop tab from preloading videos', ['injectTab', 'unpreloader', "preinject please kthx"]);
 	// ^ https://hoyois.github.io/safariextensions/universalextension/#html5_blocker
-	$.browser.tabSelected = new $.WebView({ url: "http://mobile.twitter.com", agent: "Mozilla/5.0 (iPad; CPU OS 9_3_2 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Mobile/13F69 Twitter for iPad"});
-	//"Mozilla/5.0 (iPad; CPU OS 9_3_2 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Mobile/13F69 Twitter for iPad"})
+	let agent = "Mozilla/5.0 (iPad; CPU OS 12 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/13F69 Twitter for iPad";
+	$.browser.tabSelected = new $.WebView({ url: "http://mobile.twitter.com", agent: agent})
 };
 
 let injectTab = require('app_injectTab.js');
